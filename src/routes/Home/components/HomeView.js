fd4140e048eb578aa7e23af9eed68ba1f08c791c
meirav shimelman <Meirav@grth.co.il>
2018-05-21 10:38:52 +0300

    dsgdsh

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -6,6 +6,9 @@ export const HomeView = () => (
   <div className="home1">
     <h4>Welcome EPG | GRTH!</h4>
     <img alt='This is a duck, because Redux!!!' className='duck' src={DuckImage} />
+    <p>hello</p>
+    <img alt='This is a duck, because Redux!!!' className='duck' src={DuckImage} />
+    <div>dzstg5ujsrtu</div>
   </div>
 )
 export default HomeView
